adbfb858cad095dbaea53470a64c493e53a95b70
Gurdip Singh <singh.gurdip24@gmail.com>
2020-05-23 21:37:54 -0400

    set up spring security boilerplate

---
+++ src/main/java/com/codesingh/readitlaterapp/ReaditlaterappApplication.java
@@ -0,0 +1,13 @@
+package com.codesingh.readitlaterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaditlaterappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaditlaterappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codesingh/readitlaterapp/ReaditlaterappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesingh.readitlaterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaditlaterappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
