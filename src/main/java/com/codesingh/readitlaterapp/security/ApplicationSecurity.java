f2b9a22e83b00b23d270de20aab9447673fda802
Gurdip Singh <singh.gurdip24@gmail.com>
2020-05-24 13:29:47 -0400

    authentication and authorization implemented

---
+++ src/main/java/com/codesingh/readitlaterapp/security/ApplicationSecurity.java
@@ -0,0 +1,41 @@
+package com.codesingh.readitlaterapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("myuser")
+                .password("blah")
+                .roles("User")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("Admin")
+        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+          .antMatchers("/admin").hasRole("Admin")
+          .antMatchers("/user").hasAnyRole("User","Admin")
+          .antMatchers("/**").permitAll()
+          .and().formLogin();
+    }
+
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
